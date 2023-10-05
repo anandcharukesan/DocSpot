4318472b57e93ddf8739592a94da7e01110e7403
Dhiyanesh <dhiyaneshsasi03@gmail.com>
2023-10-05 13:03:32 +0530

    doctor list append

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// main.dart
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -90,6 +92,7 @@ class _HospitalDetailsScreenState extends State<HospitalDetailsScreen> {
     final response = await http.get(Uri.parse('https://c709-117-250-228-98.ngrok-free.app/api/hospitals/${widget.hospitalId}/departments/'));
     if (response.statusCode == 200) {
       departments = jsonDecode(response.body);
+
       setState(() {});
     } else {
       throw Exception('Failed to load departments');
@@ -106,9 +109,72 @@ class _HospitalDetailsScreenState extends State<HospitalDetailsScreen> {
         itemCount: departments.length,
         itemBuilder: (BuildContext context, int index) {
           final department = departments[index];
+          print(department);
           return ListTile(
             title: Text(department['department_name']),
             subtitle: Text(department['description']),
+            onTap: () {
+              // Navigate to the DoctorListScreen with hospital and department IDs
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DoctorListScreen(
+                    hospitalId: widget.hospitalId, // Provide a default value (e.g., 0) if it's null
+                    departmentId: department['id'],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DoctorListScreen extends StatefulWidget {
+  final int hospitalId;
+  final int departmentId;
+
+  DoctorListScreen({required this.hospitalId, required this.departmentId});
+
+  @override
+  _DoctorListScreenState createState() => _DoctorListScreenState();
+}
+
+class _DoctorListScreenState extends State<DoctorListScreen> {
+  List<dynamic> doctors = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDoctors();
+  }
+
+  Future<void> fetchDoctors() async {
+
+    final response = await http.get(Uri.parse('https://c709-117-250-228-98.ngrok-free.app/api/hospitals/${widget.hospitalId}/departments/${widget.departmentId}/doctors/'));
+    if (response.statusCode == 200) {
+      doctors = jsonDecode(response.body);
+      setState(() {});
+    } else {
+      throw Exception('Failed to load doctors');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Doctor List'),
+      ),
+      body: ListView.builder(
+        itemCount: doctors.length,
+        itemBuilder: (BuildContext context, int index) {
+          final doctor = doctors[index];
+          return ListTile(
+            title: Text(doctor['doctor_name']),
+            // Add any other doctor details here
           );
         },
       ),
