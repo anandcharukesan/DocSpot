f31cab775478693cd39e796779e5b88588a4b40b
Dhiyanesh <dhiyaneshsasi03@gmail.com>
2023-10-12 13:32:03 +0530

    List size fix

--- lib/screens/hospital_details_screen.dart
+++ lib/screens/hospital_details_screen.dart
@@ -23,7 +23,6 @@ class _HospitalDetailsScreenState extends State<HospitalDetailsScreen> {
   List<dynamic> doctors = [];
   TextEditingController searchController = TextEditingController();
 
-
   @override
   void initState() {
     super.initState();
@@ -32,7 +31,8 @@ class _HospitalDetailsScreenState extends State<HospitalDetailsScreen> {
   }
 
   Future<void> fetchDepartments() async {
-    final response = await http.get(Uri.parse('$apiUrl/api/hospitals/${widget.hospitalId}/departments/'));
+    final response = await http.get(
+        Uri.parse('$apiUrl/api/hospitals/${widget.hospitalId}/departments/'));
 
     if (response.statusCode == 200) {
       departments = jsonDecode(response.body);
@@ -43,7 +43,8 @@ class _HospitalDetailsScreenState extends State<HospitalDetailsScreen> {
   }
 
   Future<void> fetchDoctors() async {
-    final response = await http.get(Uri.parse('$apiUrl/api/hospitals/${widget.hospitalId}/doctors/'));
+    final response = await http
+        .get(Uri.parse('$apiUrl/api/hospitals/${widget.hospitalId}/doctors/'));
     if (response.statusCode == 200) {
       doctors = jsonDecode(response.body);
       setState(() {});
@@ -57,7 +58,7 @@ class _HospitalDetailsScreenState extends State<HospitalDetailsScreen> {
     return DefaultTabController(
       length: 2, // Number of tabs, in this case, "Departments" and "Doctors"
       child: Scaffold(
-        backgroundColor: Color(0xFFede5ff),
+        backgroundColor: Color.fromARGB(255, 224, 214, 249),
         appBar: AppBar(
           elevation: 1,
           backgroundColor: Color(0xFFa48dd0),
@@ -77,42 +78,12 @@ class _HospitalDetailsScreenState extends State<HospitalDetailsScreen> {
               Tab(text: 'Departments'),
               Tab(text: 'Doctors'),
             ],
-            indicatorColor: Color(0xFFf8f4ff), // Set the color of the bottom indicator line
+            indicatorColor:
+                Color(0xFFf8f4ff), // Set the color of the bottom indicator line
           ),
         ),
         body: Column(
           children: [
-
-            // Padding(
-            //   padding: EdgeInsets.symmetric(horizontal: 22.0),
-            //   child: Container(
-            //
-            //     margin: EdgeInsets.only(top: 20, bottom: 5),
-            //     padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8),
-            //     decoration: BoxDecoration(
-            //       boxShadow: [
-            //         BoxShadow(
-            //           color: Colors.grey, // Shadow color
-            //           offset: Offset(0,
-            //               2), // Offset of the shadow (horizontal, vertical)
-            //           blurRadius: 8, // Blur radius of the shadow
-            //           spreadRadius: 0, // Spread radius of the shadow
-            //         ),
-            //       ],
-            //       color: Color(0xFFf8f4ff), // Background color of the search bar
-            //       borderRadius: BorderRadius.circular(17.0),
-            //     ),
-            //     child: TextField(
-            //       cursorColor: Colors.grey,
-            //       controller: searchController,
-            //       decoration: InputDecoration(
-            //         hintText: 'Search Doctors...',
-            //         border: InputBorder.none,
-            //       ),
-            //     ),
-            //   ),
-            // ),
-
             // TabBarView
             Expanded(
               child: TabBarView(
@@ -123,24 +94,38 @@ class _HospitalDetailsScreenState extends State<HospitalDetailsScreen> {
                     itemBuilder: (BuildContext context, int index) {
                       final department = departments[index];
                       return Container(
+                        margin: index == 0
+                            ? EdgeInsets.only(
+                                top: 20, bottom: 5, left: 26, right: 26)
+                            : EdgeInsets.symmetric(vertical: 5, horizontal: 26),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(17),
-                          color: Color(0xFFf8f4ff),
+                          color: Color.fromARGB(255, 242, 236, 253),
                           boxShadow: [
                             BoxShadow(
-                              color: Colors.black.withOpacity(0.25), // Shadow color
-                              offset: Offset(0, 2), // Offset of the shadow (horizontal, vertical)
-                              blurRadius: 8, // Blur radius of the shadow
+                              color: Colors.black.withOpacity(0.25),
+                              offset: Offset(0, 2),
+                              blurRadius: 8,
                               spreadRadius: 0,
-                            )
+                            ),
                           ],
                         ),
-                        margin: EdgeInsets.symmetric(vertical: 7, horizontal: 22),
                         child: Padding(
-                          padding: const EdgeInsets.all(8),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 8),
                           child: ListTile(
-                            title: Text(department['department_name'], style: TextStyle(fontWeight: FontWeight.w400,color: Color(0xFF2E2344)),),
-                            subtitle: Text(department['description'], style: TextStyle(color: Color(0xFFA38CCF)),),
+                            title: Text(
+                              department['department_name'],
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xFF2E2344),
+                              ),
+                            ),
+                            subtitle: Text(
+                              department['description'],
+                              style: TextStyle(
+                                  color: Color(0xFFA38CCF), fontSize: 12),
+                            ),
                             onTap: () {
                               Navigator.push(
                                 context,
@@ -164,24 +149,36 @@ class _HospitalDetailsScreenState extends State<HospitalDetailsScreen> {
                     itemBuilder: (BuildContext context, int index) {
                       final doctor = doctors[index];
                       return Container(
+                        margin: index == 0
+                            ? EdgeInsets.only(
+                                top: 20, bottom: 5, left: 26, right: 26)
+                            : EdgeInsets.symmetric(vertical: 5, horizontal: 26),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(17),
-                          color: Color(0xFFf8f4ff),
+                          color: Color.fromARGB(255, 242, 236, 253),
                           boxShadow: [
                             BoxShadow(
-                              color: Colors.black.withOpacity(0.25), // Shadow color
-                              offset: Offset(0, 2), // Offset of the shadow (horizontal, vertical)
-                              blurRadius: 8, // Blur radius of the shadow
+                              color: Colors.black.withOpacity(0.25),
+                              offset: Offset(0, 2),
+                              blurRadius: 8,
                               spreadRadius: 0,
-                            )
+                            ),
                           ],
                         ),
-                        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 22),
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: ListTile(
-                            title: Text(doctor['doctor_name'],style: TextStyle(fontWeight: FontWeight.w400,color: Color(0xFF2E2344)),),
-                            subtitle: Text(doctor['specialization'], style: TextStyle(color: Color(0xFFA38CCF)),),
+                            title: Text(
+                              doctor['doctor_name'],
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xFF2E2344),
+                              ),
+                            ),
+                            subtitle: Text(
+                              doctor['specialization'],
+                              style: TextStyle(color: Color(0xFFA38CCF)),
+                            ),
                             // Add onTap functionality for doctors if needed
                           ),
                         ),
@@ -197,4 +194,3 @@ class _HospitalDetailsScreenState extends State<HospitalDetailsScreen> {
     );
   }
 }
-
