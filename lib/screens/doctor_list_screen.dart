3ae650d42721f754eb99f822acdcd0ada72335cc
Dhiyanesh <dhiyaneshsasi03@gmail.com>
2023-10-12 15:41:20 +0530

    doctor-list fix

--- lib/screens/doctor_list_screen.dart
+++ lib/screens/doctor_list_screen.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../api/endPoint.dart';
 
+//////////////////////////////////////////////////////////////Doctor List///////////////////////////////////////////////////////////////////////
+
 class DoctorListScreen extends StatefulWidget {
   final int hospitalId;
   final int departmentId;
@@ -28,6 +30,7 @@ class _DoctorListScreenState extends State<DoctorListScreen> {
 
     if (response.statusCode == 200) {
       doctors = jsonDecode(response.body);
+      print(doctors);
       setState(() {});
     } else {
       throw Exception('Failed to load doctors');
@@ -39,20 +42,23 @@ class _DoctorListScreenState extends State<DoctorListScreen> {
     return Scaffold(
       backgroundColor: Color(0xFFf1eaff),
       appBar: AppBar(
-
         elevation: 0,
         backgroundColor: Colors.transparent,
         leading: IconButton(
           icon: Icon(
             Icons.arrow_back_ios,
-            color: Colors.black87,// Use the iPhone-style back arrow icon
+            color: Colors.black87, // Use the iPhone-style back arrow icon
           ),
           onPressed: () {
             // Handle back button press here
             Navigator.of(context).pop();
           },
         ),
-        title: Text('Doctor List',style: TextStyle(color: Color(0xFF755ca7), fontWeight: FontWeight.bold),),
+        title: Text(
+          'Doctor List',
+          style:
+              TextStyle(color: Color(0xFF755ca7), fontWeight: FontWeight.bold),
+        ),
       ),
       body: ListView.builder(
         itemCount: doctors.length,
