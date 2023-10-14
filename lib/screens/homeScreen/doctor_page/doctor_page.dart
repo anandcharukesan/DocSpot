5c09c3426d4bb8e97c4a10c5da63a810d708b091
Indhiraraj <indhiraraj7@gmail.com>
2023-10-14 12:03:08 +0530

    add basic appointment functionality

--- lib/screens/homeScreen/doctor_page/doctor_page.dart
+++ lib/screens/homeScreen/doctor_page/doctor_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:DocSpot/api/endPoint.dart';
 
 class DoctorDetailsScreen extends StatelessWidget {
   final Map<String, dynamic> doctor;
@@ -9,8 +12,25 @@ class DoctorDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFf1eaff),
       appBar: AppBar(
-        title: Text('Doctor Details'),
+        elevation: 1,
+        backgroundColor: Color(0xFFa48dd0),
+       leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios_new,
+              // Use the iPhone-style back arrow icon
+            ),
+            onPressed: () {
+              // Handle back button press here
+              Navigator.of(context).pop();
+            },
+          ),
+        title: Text(
+          'Doctor List',
+          // style:
+          //     TextStyle(color: Color(0xFF755ca7), fontWeight: FontWeight.bold),
+        ),
       ),
       body: Padding(
         padding: EdgeInsets.all(16),
@@ -44,23 +64,33 @@ class DoctorDetailsScreen extends StatelessWidget {
               'Hospital: $hospital',
               style: TextStyle(
                 fontSize: 20,
-                color: Color(0xFF2E2344), // Text color
+                color: Color.fromARGB(255, 135, 99, 207), // Text color
               ),
             ),
             SizedBox(height: 16),
             // Timings
             Text(
-              'Timings: 9.00 - 4.00',
+              'Timings: 9.00 - 2.00 lunch 4.00 - 8.00',
               style: TextStyle(
                 fontSize: 20,
-                color: Color(0xFF2E2344), // Text color
+                color: Color.fromARGB(255, 135, 99, 207), // Text color
               ),
             ),
             SizedBox(height: 24),
             // Book Doctor Button
             ElevatedButton(
-              onPressed: () {
-                // Implement booking functionality here
+              onPressed: () async {
+                   try {
+  final response = await http.post(Uri.parse('$apiUrl/api/bookdoctor/${doctor["id"]}'));
+  if (response.statusCode == 200) {
+    print("Request was successful: ${response.body}");
+  } else {
+    print("Request failed with status code: ${response.statusCode} ${response.body}");
+  }
+} catch (e) {
+  print("Error making the request: $e");
+}
+
               },
               child: Text(
                 'Book Doctor',
